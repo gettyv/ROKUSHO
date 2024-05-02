4f35a2bd7dacfe2563da589099c6c86b153d9c1c
Phreakester <gettyv05@gmail.com>
2024-05-02 15:13:19 -0700

    working fast constants on the track

--- include/constants.h
+++ include/constants.h
@@ -1,18 +1,19 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 constexpr int num_motors_ps = 2;
-constexpr int m_pin[][2] = {{2,3}, {6,7}, {4,5}, {8,9}};
+constexpr int m_pin[][2] = {{2,3}, {4,5}, {6,7}, {8,9}};
 
 constexpr int calibration_LED_pin = 13;
 
 constexpr const int num_line_sensors = 13;
-constexpr unsigned char line_follower_pins[num_line_sensors] = {A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12};
+constexpr unsigned char line_follower_pins[num_line_sensors] = {A12, A11, A10, A9, A8, A7, A6, A5, A4, A3, A2, A1, A0};
 constexpr const int line_center_position = 1000 * (num_line_sensors - 1) / 2;
 
 constexpr float max_speed = 10;
-constexpr float base_speed = 7;
-constexpr float Kp = 0.005;
-constexpr float Kd = 0.0008;
+constexpr float base_speed = 8;
+constexpr float turn_speed = 1;
+constexpr float Kp = 0.02;
+constexpr float Kd = 0.001;
 
 constexpr int calibration_iterations = 100;
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@
 #include <controller.h>
 
 QTRSensors lf;
-Controller controller(Kp, Kd);
+Controller base_controller(Kp, Kd);
 Motor motors[] = {Motor(m_pin[0][0], m_pin[0][1]), 
 Motor(m_pin[1][0], m_pin[1][1]), 
 Motor(m_pin[2][0], m_pin[2][1]), 
@@ -27,6 +27,101 @@ T clamp(T value, T minVal, T maxVal) {
   }
 }
 
+void go_forward_for_time(int duration) {
+  unsigned long start_time = millis();
+  while (millis() - start_time < duration) {
+    motors[0].set_speed(base_speed);
+    motors[1].set_speed(base_speed);
+    motors[2].set_speed(-base_speed);
+    motors[3].set_speed(-base_speed);
+  }
+  motors[0].set_speed(0);
+  motors[1].set_speed(0);
+  motors[2].set_speed(0);
+  motors[3].set_speed(0);
+}
+
+void turn_right() {
+  uint16_t sensors[num_line_sensors];
+  int16_t previous_position = lf.readLineBlack(sensors);
+  
+  motors[0].set_speed(turn_speed);
+  motors[1].set_speed(turn_speed);
+  motors[2].set_speed(-turn_speed);
+  motors[3].set_speed(-turn_speed);
+
+  bool is_decreasing = true;
+  while (is_decreasing) {
+    Serial.println("Decreasing position");
+    int16_t position = lf.readLineBlack(sensors);
+
+    // Check if the readlineblack value is decreasing
+    if (position >= previous_position) {
+      is_decreasing = false;
+    }
+    previous_position = position;
+  }
+
+  // Now the average should be around the center, then snap to the right when it hits the line
+  while (true) {
+    Serial.println("Can't find position");
+    int16_t position = lf.readLineBlack(sensors);
+    if (position > line_center_position + 3000) {
+      break;
+    }
+  }
+
+  // Now just need to control to be onto the line
+  while (true) {
+    Serial.println("Final Turn Stretch");
+    int16_t position = lf.readLineBlack(sensors);
+    if (abs(position - line_center_position) < 500) {
+      break;
+    }
+  }
+  motors[0].set_speed(0);
+  motors[1].set_speed(0);
+  motors[2].set_speed(0);
+  motors[3].set_speed(0);
+}
+
+void line_follow_until_junction(Controller controller) {
+  while (true)
+  {
+    uint16_t sensors[num_line_sensors];
+    int16_t position = lf.readLineBlack(sensors);
+    
+    // If all sensors see very low reflectance, take some appropriate action
+    // for this situation.
+    bool allSensorsLowReflectance = true;
+    for (int i = 0; i < num_line_sensors; i++) {
+      if (sensors[i] > 750) {
+        allSensorsLowReflectance = false;
+        break;
+      }
+    }
+    
+    if (allSensorsLowReflectance) return;
+
+    int error = position - line_center_position;
+    float controller_output = controller.update(error);
+
+    int left_speed = clamp(base_speed + controller_output, -max_speed, max_speed);
+    int right_speed = clamp(base_speed - controller_output, -max_speed, max_speed);
+
+    motors[0].set_speed(-left_speed);
+    motors[1].set_speed(-left_speed);
+    motors[2].set_speed(-right_speed);
+    motors[3].set_speed(right_speed);
+
+    Serial.print(position);
+    Serial.print(",");
+    Serial.println(controller_output);
+  }
+}
+
+
+
 void setup() {
   Serial.begin(9600);
   lf.setTypeAnalog();
@@ -45,52 +140,9 @@ void setup() {
 
 
 
+
 void loop() {
-  uint16_t sensors[num_line_sensors];
-  // Get calibrated sensor values returned in the sensors array, along with the
-  // line position, which will range from 0 to 2000, with 1000 corresponding to
-  // a position under the middle sensor.
-  int16_t position = lf.readLineBlack(sensors);
-  
-  // If all sensors see very low reflectance, take some appropriate action
-  // for this situation.
-  bool allSensorsLowReflectance = true;
-  for (int i = 0; i < num_line_sensors; i++) {
-    if (sensors[i] > 750) {
-      allSensorsLowReflectance = false;
-      break;
-    }
-  }
-  
-  if (allSensorsLowReflectance) {
-    if (picked_up) {
-      // This is when we are at a junction, and we have already picked up the patties
-      // Put code here for storing the patties
-      return;
-    }
-    else {
-      // This is when we are at a junction, and we have not picked up the patties
-      // Put code here for picking up the patties
-      picked_up = true;
-      return;
-    }
-    return;
-  }
-
-  int error = position - line_center_position;
-  float controller_output = controller.update(error);
-
-  int left_speed = clamp(base_speed + controller_output, -max_speed, max_speed);
-  int right_speed = clamp(base_speed - controller_output, -max_speed, max_speed);
-
-  motors[0].set_speed(left_speed);
-  // motors[1].set_speed(left_speed);
-  motors[2].set_speed(-right_speed);
-  // motors[3].set_speed(right_speed);
-
-  Serial.print(position);
-  Serial.print(",");
-  Serial.println(controller_output);
+  line_follow_until_junction(base_controller);
 }
 
 
