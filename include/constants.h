fa14c30967348e75e129c7dc0103e581728f8ffd
Nathan Ge <nzge@g.ucla.edu>
2024-05-24 16:15:58 -0700

    Claw code 5-24-24 4:16pm

--- include/constants.h
+++ include/constants.h
@@ -2,18 +2,25 @@
 #define CONSTANTS_H
 constexpr int num_motors_ps = 2;
 constexpr int m_pin[][2] = {{2,3}, {4,5}, {6,7}, {8,9}};
+constexpr int s_pin[][2] = {{12, A0}, {13, A1}};  
+
+#define LIMIT_SWITCH_PIN_L 2
+#define LIMIT_SWITCH_PIN_R 3
 
 constexpr int calibration_LED_pin = 13;
 
+//IR array
 constexpr const int num_line_sensors = 13;
 constexpr unsigned char line_follower_pins[num_line_sensors] = {A12, A11, A10, A9, A8, A7, A6, A5, A4, A3, A2, A1, A0};
 constexpr const int line_center_position = 1000 * (num_line_sensors - 1) / 2;
+constexpr int calibration_iterations = 100;
 
+//line following control
 constexpr float max_speed = 10;
 constexpr float base_speed = 8;
 constexpr float turn_speed = 1;
 constexpr float Kp = 0.02;
-constexpr float Kd = 0.005;
+constexpr float Kd = 0.001;
 
-constexpr int calibration_iterations = 100;
+
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,29 @@
 #include <Arduino.h>
 
+
 #include <motor.h>
+#include <servo.h>
 #include <constants.h>
+
 
 #include <QTRSensors.h>
 #include <controller.h>
 
+
 QTRSensors lf;
-Controller base_controller(Kp, Kd);
-Motor motors[] = {Motor(m_pin[0][0], m_pin[0][1]), 
-Motor(m_pin[1][0], m_pin[1][1]), 
-Motor(m_pin[2][0], m_pin[2][1]), 
+Controller controller(Kp, Kd);
+Motor motors[] = {Motor(m_pin[0][0], m_pin[0][1]),
+Motor(m_pin[1][0], m_pin[1][1]),
+Motor(m_pin[2][0], m_pin[2][1]),
 Motor(m_pin[3][0], m_pin[3][1])};
 
+
+Servo bar_servo = Servo(s_pin[0][0], s_pin[0][1]);
+Servo claw_servo = Servo(s_pin[1][0], s_pin[1][1]);
+
+
 bool picked_up = false;
-int turning_cycles = 0;
-int avoid_cycles = 0;
-int dropoff_location = 0;
-int dropoff_target = 2;
+
 
 // Simple clamping function
 template <typename T>
@@ -31,161 +37,165 @@ T clamp(T value, T minVal, T maxVal) {
   }
 }
 
-void go_forward_for_time(int duration) {
-  unsigned long start_time = millis();
-  while (millis() - start_time < duration) {
-    motors[0].set_speed(base_speed);
-    motors[1].set_speed(base_speed);
-    motors[2].set_speed(-base_speed);
-    motors[3].set_speed(-base_speed);
-  }
-  motors[0].set_speed(0);
-  motors[1].set_speed(0);
-  motors[2].set_speed(0);
-  motors[3].set_speed(0);
-}
 
-void turn_right() {
-  uint16_t sensors[num_line_sensors];
-  int16_t previous_position = lf.readLineBlack(sensors);
-  
-  motors[0].set_speed(turn_speed);
-  motors[1].set_speed(turn_speed);
-  motors[2].set_speed(-turn_speed);
-  motors[3].set_speed(-turn_speed);
+int SWEEP_DELAY = 10;
 
-  bool is_decreasing = true;
-  while (is_decreasing) {
-    Serial.println("Decreasing position");
-    int16_t position = lf.readLineBlack(sensors);
 
-    // Check if the readlineblack value is decreasing
-    if (position >= previous_position) {
-      is_decreasing = false;
-    }
-    previous_position = position;
-  }
+int bar_traj[]= {180, 130};
+int bar_servoPos = 0;
 
-  // Now the average should be around the center, then snap to the right when it hits the line
-  while (true) {
-    Serial.println("Can't find position");
-    int16_t position = lf.readLineBlack(sensors);
-    if (position > line_center_position + 3000) {
-      break;
-    }
-  }
 
-  // Now just need to control to be onto the line
-  while (true) {
-    Serial.println("Final Turn Stretch");
-    int16_t position = lf.readLineBlack(sensors);
-    if (abs(position - line_center_position) < 500) {
-      break;
-    }
-  }
-  motors[0].set_speed(0);
-  motors[1].set_speed(0);
-  motors[2].set_speed(0);
-  motors[3].set_speed(0);
-}
+int claw_traj[]= {40, 70};
+int claw_servoPos = 0;
 
-void line_follow_until_junction(Controller controller) {
-  while (true)
-  {
-    uint16_t sensors[num_line_sensors];
-    int16_t position = lf.readLineBlack(sensors);
-    
-    bool allLeftLowReflectance = true;
-    bool allRightLowReflectance = true;
 
-    for (int i = 0; i < num_line_sensors; i++) {
-      if (sensors[i] < 100) {
-        
-        if (i < num_line_sensors / 2) {
-          allRightLowReflectance = false;
-        } else {
-          allLeftLowReflectance = false;
-        }
-      }
-    }
+void grabDisc();
+void dropoffDisc();
 
-    int error = position - line_center_position;
-    float controller_output = controller.update(error);
 
-    float fwd_speed = base_speed;
-    if (allLeftLowReflectance && !allRightLowReflectance) {
-      turning_cycles = 50;
-    }
-    if (turning_cycles > 0) {
-      fwd_speed = 4;
-      turning_cycles--;
-    }
+int minAngle = 80;  // Minimum angle for the servo
+int maxAngle = 130;  // Maximum angle for the servo
+int startAngle_bar = bar_traj[0];  // Current angle of the servo
+int startAngle_claw = claw_traj[0];  // Current angle of the servo
 
-    // if (allRightLowReflectance && !allLeftLowReflectance) {
-    //   dropoff_location++;
-    //   if (dropoff_location == dropoff_target) {
-    //     controller_output = 30;
-    //     int left_speed = clamp(fwd_speed + controller_output, -max_speed, max_speed);
-    //     int right_speed = clamp(fwd_speed - controller_output, -max_speed, max_speed);
 
-    //     motors[0].set_speed(-left_speed);
-    //     motors[1].set_speed(-left_speed);
-    //     motors[2].set_speed(-right_speed);
-    //     motors[3].set_speed(right_speed);
-    //     delay(1000);
-    //     motors[0].set_speed(0);
-    //     motors[1].set_speed(0);
-    //     motors[2].set_speed(0);
-    //     motors[3].set_speed(0);
-    //     delay(1e1000);
-    //   }
-    //   else {
-    //     avoid_cycles = 30;
-    //   }
-    // }
-
-    // if (avoid_cycles > 0) {
-    //   controller_output = -3;
-    //   avoid_cycles--;
-    // }
-
-    int left_speed = clamp(fwd_speed + controller_output, -max_speed, max_speed);
-    int right_speed = clamp(fwd_speed - controller_output, -max_speed, max_speed);
-
-    motors[0].set_speed(-left_speed);
-    motors[1].set_speed(-left_speed);
-    motors[2].set_speed(-right_speed);
-    motors[3].set_speed(right_speed);
-
-    Serial.print(position);
-    Serial.print(",");
-    Serial.println(controller_output);
-  }
-}
-
+bool switchPressed = false;
+bool switchPressed_L = false; // Flag to track switch state change
+bool switchPressed_R = false;
 
 
 void setup() {
   Serial.begin(9600);
-  lf.setTypeAnalog();
-  lf.setSensorPins(line_follower_pins, num_line_sensors);
-
-
-  digitalWrite(calibration_LED_pin, HIGH);
-  for (uint8_t i = 0; i < calibration_iterations; i++)
-  {
-    lf.calibrate();
-    delay(20);
-    Serial.println(i);
-  }
-  digitalWrite(calibration_LED_pin, LOW);
+  bar_servo.set_angle(startAngle_bar);
+  claw_servo.set_angle(startAngle_claw);
+  pinMode(LIMIT_SWITCH_PIN_L, INPUT_PULLUP); // Configure the limit switch pin as input with internal pull-up resistor
+  pinMode(LIMIT_SWITCH_PIN_R, INPUT_PULLUP);
 }
-
-
 
 
 void loop() {
-  line_follow_until_junction(base_controller);
+
+
+  int true_angle = bar_servo.measure_angle();  // Read the sensor value from analog pin A0 (example pin)
+  int limitSwitchState_L = digitalRead(LIMIT_SWITCH_PIN_L);
+  int limitSwitchState_R = digitalRead(LIMIT_SWITCH_PIN_R);
+
+
+  if (limitSwitchState_L == LOW && !switchPressed_L) {
+    switchPressed_L = true; // Set the flag
+  }
+
+
+  if (limitSwitchState_R == LOW && !switchPressed_R) {
+    switchPressed_R = true; // Set the flag
+  }
+
+
+  if(switchPressed_L && switchPressed_R){
+    switchPressed = true;
+  }
+
+
+  if (switchPressed) {
+    grabDisc();
+    switchPressed_L = false;
+    switchPressed_R = false;
+    switchPressed = false;
+  }
 }
+
+
+
+
+void grabDisc(){
+   
+  // bring bar to platform
+  for (int angle = bar_traj[0]; angle >= bar_traj[1]; angle--) {
+    bar_servo.set_angle(angle);  // Move the servo to the current angle
+    delay(SWEEP_DELAY);  // Delay for smoother movement
+  }
+
+
+  delay(1000);
+ 
+    // grab disc
+  for (int angle = claw_traj[1]; angle >= claw_traj[0]; angle--) {
+    claw_servo.set_angle(angle);  // Move the servo to the current angle      
+    delay(SWEEP_DELAY);  // Delay for smoother movement
+  }
+
+
+  delay(1000);
+
+
+    // bring disc back to cup
+  for (int angle = bar_traj[1]; angle <= bar_traj[0]; angle++) {
+    bar_servo.set_angle(angle);  // Move the servo to the current angle
+    delay(SWEEP_DELAY);  // Delay for smoother movement
+  }
+
+
+  delay(1000);
+
+
+  // drop disc
+  for (int angle = claw_traj[0]; angle <= claw_traj[1]; angle++) {
+    claw_servo.set_angle(angle);  // Move the servo to the current angle
+    delay(SWEEP_DELAY);  // Delay for smoother movement
+  }
+
+
+  delay(1000);
+
+
+}
+
+
+void dropoffDisc(){
+
+
+    //bring bar towards patty
+    for (int angle = bar_traj[1]; angle <= bar_traj[2]; angle++) {
+      bar_servo.set_angle(angle);   // Set servo angle
+      delay(SWEEP_DELAY);     // Wait before changing angle
+    }
+ 
+    delay(500);
+
+
+    //grab patty
+    for (int angle = claw_traj[1]; angle <= claw_traj[2]; angle++) {
+      claw_servo.set_angle(angle);   // Set servo angle
+      delay(SWEEP_DELAY);     // Wait before changing angle
+    }
+
+
+    delay(500);
+
+
+    //bring bar back
+    for (int angle = bar_traj[2]; angle >= bar_traj[1]; angle--) {
+      bar_servo.set_angle(angle);   // Set servo angle
+      delay(SWEEP_DELAY);     // Wait before changing angle
+    }
+ 
+    delay(500);
+
+
+    //release patty
+    for (int angle = claw_traj[2]; angle >= claw_traj[1]; angle--) {
+      bar_servo.set_angle(angle);   // Set servo angle
+      delay(SWEEP_DELAY);     // Wait before changing angle
+    }
+}
+
+
+
+
+
+
+
+
+
 
 
